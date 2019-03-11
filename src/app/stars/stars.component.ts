810b5f5b1c3a830f7a43dd147e0420d0d432bd95
aerfajj <15738665175@163.com>
2019-03-11 18:20:58 +0800

    第一天，搭建页面

---
+++ src/app/stars/stars.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core'
+
+@Component({
+  selector: 'app-stars',
+  templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.css']
+})
+export class StarsComponent implements OnInit {
+  stars: boolean[]
+  @Input()
+  num: number = 0
+  starss: boolean[]
+  constructor() {}
+
+  ngOnInit() {
+    console.log(this.num)
+    this.starss = []
+    for (let i = 1; i <= 5; i++) {
+      this.starss.push(i > this.num)
+    }
+    console.log(this.starss)
+  }
+}
---
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-stock-manage',
+  templateUrl: './stock-manage.component.html',
+  styleUrls: ['./stock-manage.component.css']
+})
+export class StockManageComponent implements OnInit {
+  private stocks: Array<Stock>
+  constructor() {}
+  // 組件初始化會调用這個方法
+  ngOnInit() {
+    this.stocks = [
+      new Stock(1, '第一只股票', 1.99, 3.5, '这是一只很好的股票1', [
+        '互联网',
+        'IT'
+      ]),
+      new Stock(2, '第二只股票', 2.99, 1.5, '这是一只很好的股票2', [
+        '互联网',
+        'IT'
+      ]),
+      new Stock(3, '第三只股票', 3.99, 4.5, '这是一只很好的股票3', [
+        '互联网',
+        'IT'
+      ]),
+      new Stock(4, '第四只股票', 4.99, 2.5, '这是一只很好的股票4', [
+        '互联网',
+        'IT'
+      ])
+    ]
+  }
+}
+
+/**
+ * id： 股票的id
+ * name： 股票的名稱
+ * price： 股票的價格
+ * rating： 股票的股票的評級
+ * desc： 股票的描述
+ * categories： 股票的類別
+ */
+
+export class Stock {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {}
+}
